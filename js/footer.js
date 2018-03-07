409e10d7c9ea6ce55d2e0a029d3e284f4ad54828
lyang <lyangone@users.noreply.github.com>
2018-03-06 19:56:36 -0600

    Added hours to footer (#1)

--- js/footer.js
+++ js/footer.js
@@ -1,3 +1,3 @@
 document.write("<div class='footer'>");
-document.write("&copy " + new Date().getFullYear() + " IS-259");
+document.write("&copy " + new Date().getFullYear() + " IS-259" + new Date().getHours());
 document.write("</div>");
